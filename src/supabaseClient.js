f3c6e04f216b24979d0c9915c0851cd4779d2da4
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2026-01-30 15:39:52 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY // تم حذف الكلمة المكررة هنا
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase Environment Variables')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
